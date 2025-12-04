61d50ca38afc3e3915060cccc3628db060e1394b
Jim Lawless <lawless.jim@gmail.com>
2025-12-03 19:43:33 -0600

    Day 3, problem b

---
+++ day_03/day_3b.d
@@ -0,0 +1,58 @@
+import std.stdio;
+import std.conv;
+import std.file;
+import std.array;
+import std.datetime.stopwatch;
+import std.format;
+
+unittest {
+    assert(joltage("987654321111111")==987654321111L);
+    assert(joltage("234234234234278")==434234234278L);
+}
+
+void doit(string fname) {
+    auto file = File(fname,"r");
+    long sum=0;
+    foreach(line; file.byLineCopy()) {
+        sum+=joltage(line);
+    }
+    writeln(sum);
+    file.close();
+}
+
+long joltage(string line) {
+    string largest;
+    while(line.length!=12) {
+        largest="";
+        for(int i=0;i<line.length;i++) {
+            string s="";
+            for(int j=0;j<line.length;j++) {
+                if(j!=i) {
+                    s=s ~ line[j];
+
+                }
+            }
+            if(s>largest) 
+                largest=s;
+        }
+        line=largest;
+    }
+    return to!long(line);
+}
+
+void main(string[] args) {
+    if(args.length < 2) {
+        writeln("Syntax:\n\t",args[0]," filename\n");
+        return;
+    }
+    auto sw = StopWatch(AutoStart.no);
+    sw.start();
+    try {
+        doit(args[1]);
+    }
+    catch(FileException e) {
+        writeln("Exception: ", e.msg);
+    }
+    sw.stop();
+    writeln("Elapsed time: ",sw.peek());
+}
